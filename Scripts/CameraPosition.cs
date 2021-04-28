58397ec5c552f8b9965a63209d155edcbb0fb100
HyunBeen02 <78785670+HyunBeen02@users.noreply.github.com>
2021-04-28 21:55:31 +0900

    Delete Scripts directory

--- Scripts/CameraPosition.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-public class CameraPosition : MonoBehaviour
-{
-    [SerializeField] Transform target;
-    void LateUpdate()
-    {
-        transform.position = new Vector3(target.position.x, 6, target.position.z - 4.2f);
-    }
-}
